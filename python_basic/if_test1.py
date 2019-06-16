c179125704ce08f4ccaa6732eba1fe2eaa8779d0
gb-choi <lang778@naver.com>
2019-06-16 15:55:00 +0900

    내용추가

--- python_basic/if_test1.py
+++ python_basic/if_test1.py
@@ -9,9 +9,30 @@ else:
 
 '''
 
-
+'''
 money = 2000
 if money >= 3000:
     print("택시를 타고 가라")
 else:
     print("걸어가라")
+'''
+
+
+'''
+pocket = ['paper', 'cellphone', 'money']
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+
+'''
+
+pocket = ['paper', 'cellphone']
+card = True
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    if card:
+        print("택시를 타고 가라")
+    else:
+        print("걸어가라")
